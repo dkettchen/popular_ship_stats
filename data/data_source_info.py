b4393f3d599c4714af404838e8bbde8c530785db
dkettchen <dkettchen@gmail.com>
2024-07-19 20:02:17 +0100

    add doc with data source info

---
+++ data/data_source_info.py
@@ -0,0 +1,276 @@
+sources_dict = {
+    "ao3_2013": [
+        {
+            "data_set": "ao3_2013_fandoms",
+            "link": "https://archiveofourown.org/works/16911123/chapters/39730413#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2013/raw_ao3_2013_fandoms.txt",
+            "clean_data_file_path": None,
+            "year": 2013,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2013_overall_ranking",
+            "link": "https://archiveofourown.org/works/16911123/chapters/39731004#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2013/raw_ao3_2013_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2013,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2013_pairings",
+            "link": "https://archiveofourown.org/works/16911123/chapters/39730557#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2013/raw_ao3_2013_pairings.txt",
+            "clean_data_file_path": None,
+            "year": 2013,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2014": [
+        {
+            "data_set": "ao3_2014_femslash_ranking",
+            "link": "https://archiveofourown.org/works/16939224/chapters/39803130#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2014/raw_ao3_2014_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2014,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2014_overall_ranking",
+            "link": "https://archiveofourown.org/works/16939224/chapters/39802950",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2014/raw_ao3_2014_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2014,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2015": [
+        {
+            "data_set": "ao3_2015_femslash_ranking",
+            "link": "https://archiveofourown.org/works/16939503/chapters/39804153#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2015/raw_ao3_2015_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2015,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2015_overall_ranking",
+            "link": "https://archiveofourown.org/works/16939503/chapters/39803838",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2015/raw_ao3_2015_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2015,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2016": [
+        {
+            "data_set": "ao3_2016_data",
+            "link": "https://archiveofourown.org/works/16966566/chapters/39875103#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2016/raw_ao3_2016_data.txt",
+            "clean_data_file_path": None,
+            "year": 2016,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2016_femslash_ranking",
+            "link": "https://archiveofourown.org/works/16966566/chapters/39875457#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2016/raw_ao3_2016_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2016,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2016_overall_ranking",
+            "link": "https://archiveofourown.org/works/16966566/chapters/39874710",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2016/raw_ao3_2016_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2016,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2017": [
+        {
+            "data_set": "ao3_2017_data",
+            "link": "https://archiveofourown.org/works/16976706/chapters/39900936#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2017/raw_ao3_2017_data.txt",
+            "clean_data_file_path": None,
+            "year": 2017,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2017_femslash_ranking",
+            "link": "https://archiveofourown.org/works/16976706/chapters/39901077#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2017/raw_ao3_2017_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2017,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2017_overall_ranking",
+            "link": "https://archiveofourown.org/works/16976706/chapters/39900885",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2017/raw_ao3_2017_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2017,
+            "website": "ao3"
+        },       
+    ],
+    "ao3_2019": [
+        {
+            "data_set": "ao3_2017-2019_data",
+            "link": "https://archiveofourown.org/works/19963579/chapters/47258407#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2019/raw_ao3_2017-2019_data.txt",
+            "clean_data_file_path": None,
+            "year": 2019,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2019_femslash_ranking",
+            "link": "https://archiveofourown.org/works/19963579/chapters/47258509#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2019/raw_ao3_2019_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2019,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2019_overall_ranking",
+            "link": "https://archiveofourown.org/works/19963579/chapters/47258239",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2019/raw_ao3_2019_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2019,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2020": [
+        {
+            "data_set": "ao3_2020_data",
+            "link": "https://archiveofourown.org/works/25671457/chapters/62327902#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2020/raw_ao3_2020_data.txt",
+            "clean_data_file_path": None,
+            "year": 2020,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2020_femslash_ranking",
+            "link": "https://archiveofourown.org/works/25671457/chapters/62329354#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2020/raw_ao3_2020_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2020,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2020_overall_ranking",
+            "link": "https://archiveofourown.org/works/25671457/chapters/62325097",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2020/raw_ao3_2020_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2020,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2021": [
+        {
+            "data_set": "ao3_2021_data",
+            "link": "https://archiveofourown.org/works/32940190/chapters/81752827#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2021/raw_ao3_2021_data.txt",
+            "clean_data_file_path": None,
+            "year": 2021,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2021_femslash_ranking",
+            "link": "https://archiveofourown.org/works/32940190/chapters/81753859#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2021/raw_ao3_2021_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2021,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2021_overall_ranking",
+            "link": "https://archiveofourown.org/works/32940190/chapters/81752386",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2021/raw_ao3_2021_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2021,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2022": [
+        {
+            "data_set": "ao3_2022_data",
+            "link": "https://archiveofourown.org/works/40795074/chapters/102218811",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2022/raw_ao3_2022_data.txt",
+            "clean_data_file_path": None,
+            "year": 2022,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2022_femslash_ranking",
+            "link": "https://archiveofourown.org/works/40795074/chapters/102219456#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2022/raw_ao3_2022_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2022,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2022_overall_ranking",
+            "link": "https://archiveofourown.org/works/40795074/chapters/102219135#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2022/raw_ao3_2022_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2022,
+            "website": "ao3"
+        },    
+    ],
+    "ao3_2023": [
+        {
+            "data_set": "ao3_2023_data",
+            "link": "https://archiveofourown.org/works/49183780/chapters/124099495",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2023/raw_ao3_2023_data.txt",
+            "clean_data_file_path": None,
+            "year": 2023,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2023_femslash_ranking",
+            "link": "https://archiveofourown.org/works/49183780/chapters/124100707#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2023/raw_ao3_2023_femslash_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2023,
+            "website": "ao3"
+        },    
+        {
+            "data_set": "ao3_2023_overall_ranking",
+            "link": "https://archiveofourown.org/works/49183780/chapters/124100074#workskin",
+            "date_accessed": "17 July 2024",
+            "raw_data_file_path": "data/raw_data/ao3_2023/raw_ao3_2023_overall_ranking.txt",
+            "clean_data_file_path": None,
+            "year": 2023,
+            "website": "ao3"
+        }
+    ]
+} 
+# a dictionary of lists of dicts with data about each data set
+# including file paths and source links
