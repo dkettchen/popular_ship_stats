a7fdd22fa446aceb55909b3ad7f9aa4df972b4cc
dkettchen <dkettchen@gmail.com>
2024-07-27 11:27:22 +0100

    implement and test separating ranking change values

--- src/second_cleaning_stage_code/clean_all_number_values.py
+++ src/second_cleaning_stage_code/clean_all_number_values.py
@@ -53,30 +53,123 @@ def remove_commas_from_2015_2016_fics_tallies(data_list : list):
     return output_list
 
 def separate_ranking_equals(data_list : list):
+    """
+    takes a nested list of data (taken from data/first_clean_up_data/ao3_* folders)
+
+    removes the "=" from relevant ranking numbers 
+    and returns a list of lists where all ranking number values have been converted to
+    a list value in the format of [<int>, None] if the ranking was unique,
+    or [<int>, "="] if the ranking was tied
+    """
+
     new_list = [data_list[0]]
 
     for row in data_list[1:]:
         new_row = []
 
-        if type(row[0]) == str:
-            equal_value = row[0]
-            num_string = sub("=", "", equal_value)
-            num = int(num_string)
-            new_row.append([num, "="])
+        if data_list[0][0] == "#" or data_list[0][0] == "Rank":
 
-        else: 
-            new_value = [row[0], None] # to have a consistent format! AND then we can say 
-                                    # is_equal = row[0][1] \ if is_equal: 
-                                    # -> will be true if it's a "=", and false if it's none
-            new_row.append(new_value)
+            if type(row[0]) == str:
+                equal_value = row[0]
+                num_string = sub(r"=", "", equal_value)
+                num = int(num_string)
+                new_row.append([num, "="])
 
-        new_row.extend(row[1:])
+            else: 
+                new_value = [row[0], None] # to have a consistent format! AND then we can say 
+                                        # is_equal = row[0][1] \ if is_equal: 
+                                        # -> will be true if it's a "=", and false if it's none
+                new_row.append(new_value)
+
+            new_row.extend(row[1:])
+        
+        else:
+            new_row.append(row[0]) #appending "new" column
+
+            if type(row[1]) == str:
+                equal_value = row[1]
+                num_string = sub("=", "", equal_value)
+                num = int(num_string)
+                new_row.append([num, "="])
+
+            else: 
+                new_value = [row[1], None]
+                new_row.append(new_value)
+
+            new_row.extend(row[2:])
+
         new_list.append(new_row)
 
     return new_list
 
 def separate_change_symbols(data_list : list):
-    pass
+    """
+    takes a nested list of data (taken from data/first_clean_up_data/ao3_* folders, 
+    except for the 2013 overall, 2014 femslash and 2016 data files 
+    (as they are the first iteration of their data set and do not have a change value))
+
+    removes the non-numerical characters from relevant change numbers 
+    and returns a list of lists where all change number values have been converted to
+    a list value in the format of 
+    ["-", <int>] if the ship lost ranks,
+    ["+", <int>] if the ship gained ranks,
+    [None, 0] if the ship maintained its rank,
+    ["New", None] if the ship is new to the ranking,
+    or [None, None] if there was no change specified 
+    (in sets where only new entries were specified)
+    """
+
+    if data_list[0][1] == "Change": 
+        # if it's any of the sets with a Change column 
+        # (aka 2015 onward minus 2016_data)
+        new_list = [data_list[0]] #put in unchanged columns
+
+        for row in data_list[1:]:
+            new_row = [row[0]] #put in rank value
+
+            if row[1] == "New" or row[1] == "N":
+                new_value = ["New", None] #if it's new
+            elif row[1] == 0:
+                new_value = [None, 0] #if it's unchanged
+            elif type(row[1]) == int:
+                if row[1] > 0:
+                    new_value = ["+", row[1]]
+            elif type(row[1]) == str and len(row[1]) > 0:
+                if "+" in row[1]: #if it gained ranks
+                    print(row[1])
+                    plus_value = row[1]
+                    subbed_string = sub(r"\+", "", plus_value)
+                    num = int(subbed_string)
+                    new_value = ["+", num]
+                elif "-" in row[1]: #if it lost ranks
+                    minus_value = row[1]
+                    subbed_string = sub(r"-", "", minus_value)
+                    num = int(subbed_string)
+                    new_value = ["-", num]
+            else: print(row)
+            new_row.append(new_value)
+            new_row.extend(row[2:])
+            new_list.append(new_row)
+
+    elif data_list[0][0] == "New": 
+        # if it's the 2014_overall set
+        new_list = []
+        new_columns = [data_list[0][1], data_list[0][0]] # we're switching the order
+        new_columns.extend(data_list[0][2:])
+        new_list.append(new_columns)
+
+        for row in data_list[1:]:
+            new_row = [row[1]] # we're switching the order
+
+            if row[0] == None:
+                new_value = [None, None] #if no change was specified
+            elif row[0] == "***":
+                new_value = ["New", None] #if it's new
+            new_row.append(new_value)
+            new_row.extend(row[2:])
+            new_list.append(new_row)
+
+    return new_list
 
 
 
--- test/test_second_cleaning_stage_code/test_clean_numbers.py
+++ test/test_second_cleaning_stage_code/test_clean_numbers.py
@@ -4,10 +4,11 @@ from src.second_cleaning_stage_code.clean_all_number_values import (
     separate_change_symbols
 )
 from src.util_functions.retrieve_data_from_csv import read_data_from_csv
+from src.util_functions.get_file_paths import find_paths
 import pytest
 
 @pytest.fixture
-def relevant_file_paths():
+def relevant_2015_16_file_paths():
     return [
         "data/first_clean_up_data/ao3_2015/raw_ao3_2015_femslash_ranking.csv",
         "data/first_clean_up_data/ao3_2015/raw_ao3_2015_overall_ranking.csv",
@@ -16,14 +17,28 @@ def relevant_file_paths():
         "data/first_clean_up_data/ao3_2016/raw_ao3_2016_data.csv"
     ]
 
+@pytest.fixture
+def all_file_paths():
+    all_paths = find_paths("data/first_clean_up_data/")
+    return all_paths
+
+@pytest.fixture
+def change_file_paths(all_file_paths):
+    relevant_paths = []
+    for path in all_file_paths:
+        if "2013" not in path and "2014_femslash" not in path and "2016_data" not in path:
+            relevant_paths.append(path)
+    return relevant_paths
+
+
 class TestRemoveCommas:
-    def test_does_not_mutate_input_list(self):
+    def test_commas_does_not_mutate_input_list(self):
         input_list = read_data_from_csv("data/first_clean_up_data/ao3_2016/raw_ao3_2016_data.csv")
         remove_commas_from_2015_2016_fics_tallies(input_list)
         assert input_list == read_data_from_csv("data/first_clean_up_data/ao3_2016/raw_ao3_2016_data.csv")
 
-    def test_returns_list_of_lists_of_same_length(self, relevant_file_paths):
-        for path in relevant_file_paths:
+    def test_commas_returns_list_of_lists_of_same_length(self, relevant_2015_16_file_paths):
+        for path in relevant_2015_16_file_paths:
             input_list = read_data_from_csv(path)
             updated_list = remove_commas_from_2015_2016_fics_tallies(input_list)
             assert type(updated_list) == list
@@ -32,8 +47,8 @@ class TestRemoveCommas:
                 assert type(row) == list
                 assert len(row) == len(updated_list[0])
 
-    def test_returns_works_columns_turned_into_ints(self, relevant_file_paths):
-        for path in relevant_file_paths:
+    def test_commas_returns_works_columns_turned_into_ints(self, relevant_2015_16_file_paths):
+        for path in relevant_2015_16_file_paths:
             input_list = read_data_from_csv(path)
             updated_list = remove_commas_from_2015_2016_fics_tallies(input_list)
             for row in updated_list[1:]:
@@ -46,8 +61,8 @@ class TestRemoveCommas:
                 elif "overall" in path:
                     assert type(row[-3]) == int
 
-    def test_returns_other_values_unchanged(self, relevant_file_paths):
-        for path in relevant_file_paths:
+    def test_commas_returns_other_values_unchanged(self, relevant_2015_16_file_paths):
+        for path in relevant_2015_16_file_paths:
             input_list = read_data_from_csv(path)
             updated_list = remove_commas_from_2015_2016_fics_tallies(input_list)
             for row in range(len(updated_list[1:])):
@@ -65,13 +80,13 @@ class TestRemoveCommas:
                         assert updated_list[row][index] == input_list[row][index]
 
 class TestSeparateRankingEquals:
-    def test_does_not_mutate_input_list(self):
+    def test_ranks_does_not_mutate_input_list(self):
         input_list = read_data_from_csv("data/first_clean_up_data/ao3_2016/raw_ao3_2016_data.csv")
         separate_ranking_equals(input_list)
         assert input_list == read_data_from_csv("data/first_clean_up_data/ao3_2016/raw_ao3_2016_data.csv")
 
-    def test_returns_list_of_lists_of_same_length(self, relevant_file_paths):
-        for path in relevant_file_paths:
+    def test_ranks_returns_list_of_lists_of_same_length(self, all_file_paths):
+        for path in all_file_paths:
             input_list = read_data_from_csv(path)
             updated_list = separate_ranking_equals(input_list)
             assert type(updated_list) == list
@@ -80,18 +95,83 @@ class TestSeparateRankingEquals:
                 assert type(row) == list
                 assert len(row) == len(updated_list[0])
 
-    def test_returns_a_list_value_with_an_int_and_none_or_equal_sign_item(self, relevant_file_paths):
-        for path in relevant_file_paths:
+    def test_ranks_returns_a_ranking_value_with_an_int_and_none_or_equal_sign_item(self, all_file_paths):
+        for path in all_file_paths:
             input_list = read_data_from_csv(path)
             updated_list = separate_ranking_equals(input_list)
+            for column in range(len(updated_list[0])):
+                if updated_list[0][column] == "#" or updated_list[0][column] == "Rank":
+                    index = column
             for row in updated_list[1:]:
-                assert type(row[0][0]) == int
-                assert row[0][1] == None or row[0][1] == "="
+                assert type(row[index]) == list
+                assert type(row[index][0]) == int
+                assert row[index][1] == None or row[index][1] == "="
 
-    def test_returns_other_values_unchanged(self, relevant_file_paths):
-        for path in relevant_file_paths:
+    def test_ranks_returns_other_values_unchanged(self, all_file_paths):
+        for path in all_file_paths:
             input_list = read_data_from_csv(path)
             updated_list = separate_ranking_equals(input_list)
-            for row in range(len(updated_list[1:])):
-                for index in range(1, len(updated_list[row])):
+            if "2014_overall" in path:
+                indexes = [0, 2, 3, 4, 5, 6]
+                for row in range(len(updated_list[1:])):
+                    for index in indexes:
+                        assert updated_list[row][index] == input_list[row][index]
+            else: 
+                for row in range(len(updated_list[1:])):
+                    for index in range(1, len(updated_list[row])):
+                        assert updated_list[row][index] == input_list[row][index]
+
+class TestSeparateRankingChange:
+    def test_change_does_not_mutate_input_list(self):
+        input_list = read_data_from_csv("data/first_clean_up_data/ao3_2016/raw_ao3_2016_femslash_ranking.csv")
+        separate_change_symbols(input_list)
+        assert input_list == read_data_from_csv("data/first_clean_up_data/ao3_2016/raw_ao3_2016_femslash_ranking.csv")
+
+    def test_change_returns_list_of_lists_of_same_length(self, change_file_paths):
+        for path in change_file_paths:
+            input_list = read_data_from_csv(path)
+            updated_list = separate_change_symbols(input_list)
+            assert type(updated_list) == list
+            assert len(updated_list) == len(input_list)
+            for row in updated_list:
+                assert type(row) == list
+                assert len(row) == len(updated_list[0])
+
+    def test_change_returns_first_value_as_rank_and_second_value_as_change(self, change_file_paths):
+        for path in change_file_paths:
+            input_list = read_data_from_csv(path)
+            updated_list = separate_change_symbols(input_list)
+            assert updated_list[0][0] == "Rank" or updated_list[0][0] == "#"
+            assert updated_list[0][1] == "Change" or updated_list[0][1] == "New"
+            for row in updated_list[1:]:
+                change = row[1]
+                assert type(change) == list
+
+    def test_change_returns_a_ranking_value_with_expected_values(self, change_file_paths):
+        for path in change_file_paths:
+            input_list = read_data_from_csv(path)
+            updated_list = separate_change_symbols(input_list)
+            for row in updated_list[1:]:
+                print(row)
+                change = row[1]
+                assert change[0] in ["+", "-", "New"] or change[0] == None
+                assert type(change[1]) == int or change[1] == None
+                if change[0] == "New":
+                    assert change == ["New", None]
+                elif change[0] in ["+", "-"]:
+                    assert type(change[1]) == int
+                elif not change[0]: # if change is none
+                    assert not change[1] #must be 0 or none
+
+    def test_change_returns_other_values_unchanged(self, change_file_paths):
+        for path in change_file_paths:
+            input_list = read_data_from_csv(path)
+            updated_list = separate_change_symbols(input_list)
+            for row in range(len(updated_list)):
+                if "2014_overall" not in path:
+                    assert updated_list[row][0] == input_list[row][0]
+                else: 
+                    assert updated_list[row][0] == input_list[row][1]
+                for index in range(2, len(updated_list[row])):
                     assert updated_list[row][index] == input_list[row][index]
+
