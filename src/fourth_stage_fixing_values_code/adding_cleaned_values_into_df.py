d02bed385685b4228500447df04a20555ab82a02
dkettchen <dkettchen@gmail.com>
2024-08-21 17:58:09 +0100

    make stage 4 files and test file to verify them

--- src/fourth_stage_fixing_values_code/adding_cleaned_values_into_df.py
+++ src/fourth_stage_fixing_values_code/adding_cleaned_values_into_df.py
@@ -1,26 +1,61 @@
 from src.util_functions.get_file_paths import find_paths
-from src.util_functions.attempting_pandas import json_list_of_dicts_to_data_frame
-import pandas as pd
-from json import load
+from src.fourth_stage_fixing_values_code.extracting_clean_abbr_name_lists import (
+    extract_clean_character_names, 
+    extract_clean_fandom_names
+)
+from json import load, dump
+from copy import deepcopy
+
+def run_stage_4_cleaning(): 
+    abbr_names_dict = extract_clean_character_names()
+    abbr_fandoms_dict = extract_clean_fandom_names()
+
+    all_paths = find_paths("data/third_clean_up_data/")
+
+    for path in all_paths: # going through all the files
+        with open(path, "r") as data_file:
+            dict_list = load(data_file) # list of dicts
+
+        new_list = []
+        for row in dict_list: # row is a dict
+
+            new_row = deepcopy(row) # transferring all values
+
+            for fandom in abbr_fandoms_dict:
+                if row["Fandom"] in abbr_fandoms_dict[fandom]:
+                    new_row["Old Fandom"] = new_row["Fandom"] # storing prior version
+                    new_row["Fandom"] = fandom # replacing fandom
+                    break # we found fandom, we don't need to keep looking
+                elif row["Fandom"] == 'Star Wars Story (2016)':
+                    new_row["Old Fandom"] = "Rogue One: A Star Wars Story (2016)"
+                    new_row["Fandom"] = "Star Wars"
+                    
+            
+            new_row["Old Characters"] = new_row["Relationship"] # renaming, keeping order intact
+            new_row["Relationship"] = [] # emptying list
 
 
-#TODO: 
-# -use regular reader (literal json.load) on cleaned fandoms & characters files, 
-#  then put into a df (each)
+            for character in row["Relationship"]:
+                for new_character in abbr_names_dict[new_row["Fandom"]]:
+                    if character in abbr_names_dict[new_row["Fandom"]][new_character]["op_versions"]:
+                        new_row["Relationship"].append(new_character)
+                        new_row["RPF or Fic"] = abbr_names_dict[new_row["Fandom"]][new_character]["rpf_or_fic"]
+                            # should be same label for all char involved if I did my job right
+                        break # should only break innermost loop, so continue to next char
+        
+            new_row["Relationship"] = sorted(new_row["Relationship"]) # sorting alphabetically
 
-# -get all third stage file paths (= main files) to loop through
-# -use dataframe reader on main files
+            new_list.append(new_row)
+        
+        folder_name = path[25:33]
+        specific_file = path[34:-5]
 
-# -use dfs to add a clean fandom column to the main df/possibly rename the old one
-#    -> we're keeping the original tags for later reference to identify specifics where needed
-# -also add a RPF/fictional column while we're here
-# -use dfs to add a clean names column to the main df/possibly rename the old one
+        file_name = f"data/fourth_clean_up_data/{folder_name}/{specific_file}.json"
 
-# -test result thoroughly
-
-# -print resulting data sets to stage four files (json)
+        with open(file_name, "w") as target_file:
+            dump(new_list, target_file, indent=4)
 
 
-# we don't need to run many funcs in here bc we're cleaning the data 
-# & storing the cleaned data in separate files
-#   -> we access said files instead of running cleaner functions in our run func
+if __name__ == "__main__":
+    run_stage_4_cleaning()
+    pass
---
+++ test/test_fourth_cleaning_stage_code/test_main_data.py
@@ -0,0 +1,22 @@
+
+
+    # I can already see we're missing minecrafter rpf from our abbr char list :l
+    # dunno where they got lost in the cleaning process, we'll have to fix that
+        # it did update "Youtube" as their fandom, so the fandoms are fine
+
+# check the number of rows remains unchanged
+# check there's the expected number of keys (old keys + 3)
+# check everything's the expected type etc
+
+# check fandom is in our clean fandom list (make list?)
+
+# check relationship key is not empty
+# check relationship value only contains names in our full names list (make list?)
+
+# check there is a rpf/fic key
+# check its value is either "RPF" or "fictional"
+
+# check new "old" keys have been added
+# check old keys contain original values
+
+# check remaining keys & values have not been changed
