6814a51c31cf01c16592b817190e6d2916f8fc60
dkettchen <dkettchen@gmail.com>
2024-08-29 20:51:02 +0100

    create file for data sets table

---
+++ src/fifth_cleaning_stage_code/run_stage_5_cleaning.py
@@ -0,0 +1,28 @@
+#TODO:
+# data set table: ✅
+# - go through (sorted?) list of filepaths ✅
+# - collect:
+    # - year ✅
+    # - type ✅
+# - set website to be "AO3" for all these ones ✅
+
+# characters table:
+# (they all have their fandom in their dicts, so don't need to be classified by those)
+# - un-categorise them -> we want a list of char names (as keys w dict values)
+    # - I think we have a few names that are doubled (like "Ruby"), 
+    # so maybe have the keys as like "name from fandom" or something
+    # -> we can then either split by " from " or simply query the 
+    # full_name/fandom key inside the value
+# - add rpf or fic value
+
+# main sets:
+# - sort characters in relationship alphabetically
+# - get number of characters in relationship
+# - de-list characters (?) -> where less than max num "none" for other characters
+# - remove gender from type column - replace with gen or slash only
+# - remove race column
+# - make rank a simple number
+    # possibly add a "tied" column???
+# - make change a simple value (ie pos. num, neg. num, "new", none)
+# - add data set column
+
---
+++ src/util_functions/make_data_set_table.py
@@ -0,0 +1,99 @@
+from json import dump
+
+def make_data_set_table(ordered_path_dict):
+    """
+    takes a dictionary with year number keys and list of strings values 
+    containing filepaths to our main datasets
+
+    returns a dictionary with keys labelled after and dict values 
+    containing the website (currently only ao3), year, and type
+    (currently only femslash, overall and data) of the dataset in question
+    """
+
+    all_data_sets = {}
+    for year in ordered_path_dict:
+        for path in ordered_path_dict[year]:
+            set_type = None
+            if "femslash" in path:
+                set_type = "femslash"
+            elif "overall" in path:
+                set_type = "overall"
+            else:
+                set_type = "data"
+
+            website = None
+            if "ao3" in path:
+                website = "AO3"
+            
+            data_set_info = {
+                "year" : year,
+                "type" : set_type,
+                "website" : website,
+            }
+
+            key_name = f"{website}_{year}_{set_type}"
+
+            all_data_sets[key_name] = data_set_info
+
+    sorted_data_sets = {}
+    sorted_keys = sorted(list(all_data_sets.keys()))
+    for key in sorted_keys:
+        sorted_data_sets[key] = all_data_sets[key]
+
+    return sorted_data_sets
+
+
+
+if __name__ == "__main__":
+    all_ordered_paths = { 
+        2023: [
+            'data/fourth_clean_up_data/ao3_2023/raw_ao3_2023_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2023/raw_ao3_2023_data.json', 
+            'data/fourth_clean_up_data/ao3_2023/raw_ao3_2023_overall_ranking.json',
+        ],
+        2022: [
+            'data/fourth_clean_up_data/ao3_2022/raw_ao3_2022_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2022/raw_ao3_2022_data.json', 
+            'data/fourth_clean_up_data/ao3_2022/raw_ao3_2022_overall_ranking.json',
+        ],
+        2021: [
+            'data/fourth_clean_up_data/ao3_2021/raw_ao3_2021_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2021/raw_ao3_2021_data.json', 
+            'data/fourth_clean_up_data/ao3_2021/raw_ao3_2021_overall_ranking.json',
+        ],
+        2020: [
+            'data/fourth_clean_up_data/ao3_2020/raw_ao3_2020_femslash_ranking.json',
+            'data/fourth_clean_up_data/ao3_2020/raw_ao3_2020_data.json',  
+            'data/fourth_clean_up_data/ao3_2020/raw_ao3_2020_overall_ranking.json',
+        ], 
+        2019: [
+            'data/fourth_clean_up_data/ao3_2019/raw_ao3_2019_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2019/raw_ao3_2017-2019_data.json', 
+            'data/fourth_clean_up_data/ao3_2019/raw_ao3_2019_overall_ranking.json',
+        ], 
+        2017: [
+            'data/fourth_clean_up_data/ao3_2017/raw_ao3_2017_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2017/raw_ao3_2017_data.json', 
+            'data/fourth_clean_up_data/ao3_2017/raw_ao3_2017_overall_ranking.json',
+        ], 
+        2016: [
+            'data/fourth_clean_up_data/ao3_2016/raw_ao3_2016_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2016/raw_ao3_2016_data.json', 
+            'data/fourth_clean_up_data/ao3_2016/raw_ao3_2016_overall_ranking.json',
+        ], 
+        2015: [
+            'data/fourth_clean_up_data/ao3_2015/raw_ao3_2015_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2015/raw_ao3_2015_overall_ranking.json',
+        ], 
+        2014: [
+            'data/fourth_clean_up_data/ao3_2014/raw_ao3_2014_femslash_ranking.json', 
+            'data/fourth_clean_up_data/ao3_2014/raw_ao3_2014_overall_ranking.json',
+        ], 
+        2013: [
+            'data/fourth_clean_up_data/ao3_2013/raw_ao3_2013_overall_ranking.json',
+            ],
+    }
+    data_set_dict = make_data_set_table(all_ordered_paths)
+    filepath = "data/reference_and_test_files/data_sets.json"
+    with open(filepath, "w") as file:
+        dump(data_set_dict, file, indent=4)
