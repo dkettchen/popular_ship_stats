4395b01a0e8e7237363d1dda00e71a0916cd7008
dkettchen <dkettchen@gmail.com>
2024-09-16 14:14:12 +0100

    start file to visualise femslash rankings

---
+++ visualisation/vis_femslash_rankings_file.py
@@ -0,0 +1,84 @@
+from visualisation.vis_utils.make_file_dfs import make_femslash_dfs, make_characters_df, make_ships_df
+from visualisation.vis_utils.remove_member_columns import remove_members_from_df
+from visualisation.vis_utils.join_member_info import join_character_info_to_df
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def edit_femslash_df_columns(femslash_df_dict):
+    """
+    takes output dict from make_femslash_dfs
+
+    returns a new dict with new dataframes, where the "new_works", "release_date", 
+    and "data_set" columns have been removed, a "year" column has been added, and in the 
+    2014 set, the "change" column has been filled with "new" rather than None values
+    """
+
+    new_df_dict = {}
+
+    for year in femslash_df_dict:
+        new_df = femslash_df_dict[year].copy()
+
+        # drop new works cause it's an overall ranking
+        new_df.pop("new_works")
+
+        # and release date cause we're not tracking that yet
+        new_df.pop("release_date")
+
+        # and replace data set name column with just year cause it's all femslash here for now
+        new_df.pop("data_set")
+        new_df["year"] = year
+
+        if year == 2014:
+            new_df["change"] = "new" # getting rid of none values
+
+        new_df_dict[year] = new_df
+
+    return new_df_dict
+
+def join_ship_info_to_femslash(femslash_df_dict):
+    """
+    takes femslash df dict
+
+    combines all femslash dfs into one big femslash ranking df and joins "fandom", 
+    "rpf_or_fic", "gender_combo", "race_combo", and the 4 "member_" columns from ships file onto their 
+    respective ranked ships
+    """
+    ships_df = make_ships_df().get([
+        "slash_ship",
+        "fandom",
+        "rpf_or_fic",
+        "gender_combo",
+        "race_combo",
+        "member_1",
+        "member_2",
+        "member_3",
+        "member_4",
+    ]).set_index("slash_ship")
+    
+    femslash_dfs_list = [femslash_df_dict[year] for year in femslash_df_dict]
+    full_femslash_df = pd.concat(femslash_dfs_list)
+
+    joined_df = full_femslash_df.join(other=ships_df, on="ship", lsuffix="_left", rsuffix="_right")
+
+    return joined_df
+
+
+if __name__ == "__main__":
+
+    # get data
+    femslash_df_dict = make_femslash_dfs()
+
+    # fix columns
+    new_femslash_df_dict = edit_femslash_df_columns(femslash_df_dict)
+    # combine into one big df
+    ship_joined_femslash_df = join_ship_info_to_femslash(new_femslash_df_dict) 
+
+    # make useable dfs
+    femslash_ship_info_df = remove_members_from_df(ship_joined_femslash_df)
+    femslash_character_info_df = join_character_info_to_df(ship_joined_femslash_df)
+
+
+
