6a76435dcb12ca5ad1742666bbda2c3317731125
dkettchen <dkettchen@gmail.com>
2024-10-14 15:02:14 +0100

    make constants to import

---
+++ visualisation/vis_utils/diagram_utils/colour_palettes.py
@@ -0,0 +1,8 @@
+sapphic_table = { # full sapphic tables
+    "line": 'deeppink', # colour of lines
+    "header": 'lightsalmon', # colour of header row
+    "body": 'mistyrose', # colour of remaining rows
+    "body_2": 'papayawhip', # to go with non_white header colours
+}
+
+non_white_colours = ["skyblue", "lightseagreen", "teal", "darkslategrey"]
---
+++ visualisation/vis_utils/diagram_utils/labels.py
@@ -0,0 +1,11 @@
+non_white_categories = [
+    "white_involved_ship", 
+    "e_asian_involved_ship", 
+    "non_white_ship", 
+    "non_white_or_ea_ship"
+]
+
+suffixes = {
+    "femslash" : " (AO3 femslash ranking 2014-2023)",
+    "total": " (AO3 2013-2023)"
+}
