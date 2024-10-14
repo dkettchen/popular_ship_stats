c7ec42eaa51be7716bb51e5231d520a682e989a5
dkettchen <dkettchen@gmail.com>
2024-10-14 15:08:12 +0100

    remove comment

--- visualisation/vis_utils/diagram_utils/visualise_bars.py
+++ visualisation/vis_utils/diagram_utils/visualise_bars.py
@@ -3,7 +3,6 @@ import plotly.graph_objects as go
 import visualisation.vis_utils.diagram_utils.colour_palettes as colour_palettes
 import visualisation.vis_utils.diagram_utils.labels as lbls
 
-# no multi plots but would need title adjusted
 def visualise_non_white_counts(input_df:pd.DataFrame, ranking:str):
     """
     visualises the output from count_non_white_ships (ranking=(currently implemented:)"femslash") 
@@ -14,7 +13,7 @@ def visualise_non_white_counts(input_df:pd.DataFrame, ranking:str):
     text = ["involve white ppl", "involve east asians", "non-white ships", "non-white & non-EA"]
     colours = colour_palettes.non_white_colours
     labels = [str(year)[:-2] for year in input_df.index]
-    
+
     counter = 0
 
     suffix = lbls.suffixes[ranking]
