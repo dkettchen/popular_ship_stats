879a834a177f67e9312be71b946fe2b304a88a5e
dkettchen <dkettchen@gmail.com>
2024-10-17 00:37:23 +0100

    add umbrella groupings for racial groups and gender combo types to labels

--- visualisation/vis_utils/diagram_utils/labels.py
+++ visualisation/vis_utils/diagram_utils/labels.py
@@ -15,3 +15,59 @@ suffixes = {
     "femslash" : " (AO3 femslash ranking 2014-2023)",
     "total": " (AO3 2013-2023)"
 }
+
+racial_group_umbrellas = { # racial group umbrella groups
+    "north, west, middle and eastern europe": [
+        'White', 
+        'White (Multi)',
+        'Romani', 
+        'Eu Ind (Multi)', 
+    ],
+    "black (incl afro-latin)": [
+        'Black', 
+        'Black (Multi)',
+        'Af Lat', 
+    ],
+    "south europe and (rest of) latin": [
+        'Latin',
+        'Latin (Multi)', 
+        'SE Eu', 
+        'SE Eu (Multi)', 
+    ],
+    "middle-east and north-africa": [
+        'MENA', 
+        'MENA (Multi)', 
+    ],
+    "east asia": [
+        'E Asian', 
+        'E Asian (Multi)', 
+    ],
+    "(rest of) asia": [
+        'S Asian', 
+        'S Asian (Multi)',
+        'SE Asian', 
+        'SE Asian (Multi)', 
+        'As Ind',
+        'As Ind / S Asian (Multi)', 
+        'Asian (Multi)', 
+        'Central As', 
+    ],
+    "american & polynesian indigenous": [
+        'Am Ind', 
+        'Am Ind / E Asian (Multi)', 
+        'Māori Ind',
+        'Māori Ind (Multi)', 
+    ],
+    "other": {
+        'Ambig': "ambiguous or differing casting", 
+        'N.H.': "non-human", 
+        'Unknown': "unknown",
+    },
+}
+
+gender_combo_umbrellas = { # gender combo umbrella groups
+    "mlm": ["M / M", "M / M | Other","M | Other / M / M",],
+    "wlw": ["F / F","F | Other / F | Other", "F / F | Other",],
+    "non-same-sex": ["M / F","F / Other","M / Other","F / M / M"],
+    "ambiguous": ["M / Ambig","M | Other / Ambig", "F / Ambig","M | F | Other / M | F | Other"]
+}
