d6dc83f1822aa72cdac1690653928054f50ede87
dkettchen <dkettchen@gmail.com>
2024-10-19 14:33:24 +0100

    add functionality to rename gender combo column instead of index only

--- visualisation/vis_utils/rename_gender_combos.py
+++ visualisation/vis_utils/rename_gender_combos.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-def rename_gender_combos(input_item:pd.DataFrame|pd.Series):
+def rename_gender_combos(input_item:pd.DataFrame|pd.Series, column=None):
     """
-    takes a pandas item with an index (series or df) with gender combo index labels
+    takes a pandas item with an index (series or df) or a "gender_combo" column of gender combo labels
 
-    returns a new item with its index labels renamed to match their equivalent labels
+    defaults to renaming the index
+
+    if a truthy value is given for column=, it will instead rename the "gender_combo" column
+
+    returns a new item with its relevant gender combo labels renamed to match the other labels
     """
 
     renaming_dict = {
@@ -14,6 +18,15 @@ def rename_gender_combos(input_item:pd.DataFrame|pd.Series):
         "M | Other / M": "M / M | Other"
     }
 
-    new_item = input_item.rename(index=renaming_dict)
+    if column:
+        new_item = input_item.copy()
+        new_list = []
+        for combo in new_item["gender_combo"]:
+            if combo in renaming_dict.keys():
+                new_list.append(renaming_dict[combo])
+            else: new_list.append(combo)
+        new_item["gender_combo"] = new_list
+
+    else: new_item = input_item.rename(index=renaming_dict)
 
     return new_item
