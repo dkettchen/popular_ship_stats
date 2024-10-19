3ea6099ae4968bbdbd81316b24ce5fe8dbc560be
dkettchen <dkettchen@gmail.com>
2024-10-19 12:33:16 +0100

    remove old code for make_femslash_dfs

--- visualisation/vis_utils/make_file_dfs.py
+++ visualisation/vis_utils/make_file_dfs.py
@@ -1,21 +1,5 @@
 from visualisation.vis_utils.read_csv_to_df import df_from_csv
 
-def make_femslash_dfs():
-    """
-    returns a dictionary with year keys and dataframe values
-
-    each dataframe is the corresponding year's ao3 femslash ranking data as read 
-    from the data/fifth_clean_up_data/ folder
-    """
-    years = [2014,2015,2016,2017,2019,2020,2021,2022,2023]
-
-    femslash_df_dict = {}
-    for year in years:
-        filepath = f"data/fifth_clean_up_data/ao3_{year}/stage_5_ao3_{year}_femslash.csv"
-        femslash_df_dict[year] = df_from_csv(filepath)
-
-    return femslash_df_dict
-
 def make_characters_df():
     """
     returns a dataframe with the read data from data/fifth_clean_up_data/stage_5_characters.csv
