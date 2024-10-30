47b22be8655f544cf7de5b87825b1f25166d4f70
dkettchen <dkettchen@gmail.com>
2024-10-30 15:29:19 +0000

    make sure subplot helper works for prior functions after refactor

--- visualisation/vis_utils/diagram_utils/make_subplots_by_year.py
+++ visualisation/vis_utils/diagram_utils/make_subplots_by_year.py
@@ -1,6 +1,6 @@
 from plotly.subplots import make_subplots
 
-def make_subplots_by_year(num_of_years:int, num_of_columns:int=False):
+def make_subplots_by_year(num_of_years:int, num_of_columns:int=False, by_years:bool=False):
     """
     makes a subplot figure with the appropriate layout per number of years given
 
@@ -8,17 +8,24 @@ def make_subplots_by_year(num_of_years:int, num_of_columns:int=False):
     - 3x3 grid for 9 years (type: domain)
     - 2x5 grid for 10 years (type: domain)
     - if num_of_columns provided: 
+        if by_years is True:
         - num_of_years x num_of_columns grid (type: table)
+        otherwise:
+        - num_of_years/num_of_columns (rounded up) x num_of_columns grid (type: table)
     """
 
-    if num_of_columns:
+    if num_of_columns and not by_years: # if we want to distribute the years across custom column num
         row = num_of_years / num_of_columns
         if row != int(row):
             row += 1
         row = int(row)
         column = num_of_columns
         type_dict = {"type": "table"}
-    else:
+    elif num_of_columns: # if we want a years x columns grid
+        row = num_of_years
+        column = num_of_columns
+        type_dict = {"type": "table"}
+    else: # preset grid shapes per year num (type:domain)
         if num_of_years == 9:
             row = 3
             column = 3
