f56ab7c101c935e9edad53e3066e722f88258e5c
dkettchen <dkettchen@gmail.com>
2024-11-06 12:17:24 +0000

    add doc string

--- visualisation/vis_utils/read_csv_to_df.py
+++ visualisation/vis_utils/read_csv_to_df.py
@@ -2,6 +2,11 @@ from src.util_functions.retrieve_data_from_csv import read_data_from_csv
 import pandas as pd
 
 def df_from_csv(filepath:str):
+    """
+    reads from filepath (must be csv file) (using read_data_from_csv util function)
+
+    returns a df with the data contained, using the first row as its column titles
+    """
     csv_data = read_data_from_csv(filepath)
     df = pd.DataFrame(csv_data[1:], columns=csv_data[0])
     return df
