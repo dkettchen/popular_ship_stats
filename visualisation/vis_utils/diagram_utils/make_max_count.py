3f10422dd24acdde2e59c838b5e21a3ba6e34b83
dkettchen <dkettchen@gmail.com>
2024-11-06 12:23:11 +0000

    update doc string

--- visualisation/vis_utils/diagram_utils/make_max_count.py
+++ visualisation/vis_utils/diagram_utils/make_max_count.py
@@ -3,7 +3,7 @@ def make_max_count(num_of_years:int):
     determines max count (number of columns) depending on the number of years passed in (to match 
     the relevant sub plot layout)
 
-    currently implemented numbers of years: 9 & 10
+    currently implemented numbers of years: 7, 8, 9 & 10
     """
 
     if num_of_years == 9:
