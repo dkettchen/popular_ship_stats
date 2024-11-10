54f1310def70cd1d2aee7307393f0298e9d1072f
dkettchen <dkettchen@gmail.com>
2024-11-10 09:47:38 +0000

    add countries and languages to fandoms

--- src/additional_data_fandoms/add_countries_and_languages.py
+++ src/additional_data_fandoms/add_countries_and_languages.py
@@ -1,36 +1,294 @@
 from copy import deepcopy
 
-american_fandoms = []
-british_fandoms = []
-japanese_fandoms = []
-korean_fandoms = []
-chinese_fandoms = []
-other_fandoms = {}
+american_fandoms = [
+    "Addam's Family Universe",
+    "Adventure Time",
+    "American Horror Story",
+    "Avatar: The last Airbender Universe",
+    "Bridgerton",
+    "Buffy Universe",
+    "Call of Duty",
+    "Castle",
+    "Chilling Adventures of Sabrina",
+    "Criminal Minds",
+    "Critical Role",
+    "DC",
+    "Frozen",
+    "Ghostbusters",
+    "Glee",
+    "Gravity Falls",
+    "Grey's Anatomy",
+    "Hamilton",
+    "Hannibal",
+    "Hawaii Five-0",
+    "Hazbin Hotel",
+    "House M.D.",
+    "IT",
+    "Inception",
+    "Kim Possible",
+    "Law & Order",
+    "Maleficent",
+    "Marvel",
+    "NCIS",
+    "Once Upon a Time",
+    "Orange is the New Black",
+    "Pitch Perfect",
+    "Riverdale",
+    "Roswell New Mexico",
+    "Sanders Sides",
+    "Schitt's Creek",
+    "She-Ra and the Princesses of Power",
+    "South Park",
+    "Star Trek",
+    "Star Wars",
+    "Steven Universe",
+    "Stranger Things",
+    "Suits",
+    "Supernatural",
+    "Teen Wolf",
+    "Teenage Mutant Ninja Turtles",
+    "The 100",
+    "The 100 / The Walking Dead - crossover fanon",
+    "The Big Bang Theory",
+    "The Walking Dead",
+    "Top Gun",
+    "Twilight",
+    "Warehouse 13",
+    "9-1-1", # american
+    "A Song of Ice and Fire / Game of Thrones Universe", # american
+    "Adam Lambert", # RPF american
+    "Amphibia", # american
+    "Battlestar Galactica", # american
+    "Be More Chill", # american
+    "Carry On", # american
+    "Check Please!", # american
+    "Descendants", # american
+    "Faking It", # american
+    "Fall Out Boy", # american
+    "Fifth Harmony", # american
+    "Generation Kill", # american
+    "Girl Meets World", # american
+    "Homestuck", # american
+    "Hunger Games / Panem Universe", # american
+    "Julie and The Phantoms", # american
+    "Legend of the Seeker", # american
+    "Lizzie Bennet Diaries", # american 
+    "Lucifer", # american 
+    "My Chemical Romance", # american 
+    "Our Flag Means Death", # american 
+    "Overwatch", # american 
+    "Pacific Rim", # american 
+    "Panic! at the Disco", # american 
+    "Percy Jackson", # american 
+    "Person of Interest", # american 
+    "Queer as Folk", # us version is in ranking
+    "RWBY", # american 
+    "Raven Cycle", # american 
+    "Rizzoli & Isles", # american 
+    "Shadow and Bone", # american 
+    "Shadowhunters | Mortal Instruments", # american 
+    "Shameless", # us version
+    "Six of Crows", # american (author is israeli american but it's published in america)
+    "Spartacus", # american 
+    "Stargate", # american 
+    "Starsky & Hutch", # american 
+    "Station 19", # american 
+    "The Closer", # american 
+    "The Devil Wears Prada", # american 
+    "The Good Wife", # american 
+    "The Haunting of Bly Manor", # american 
+    "The Last of Us", # american 
+    "The Man From U.N.C.L.E.", # american 
+    "The Old Guard", # american 
+    "The Owl House", # american 
+    "The Rookie", # american 
+    "The Sentinel", # american 
+    "The Wilds", # american 
+    "The X-Files", # american 
+    "Twenty One Pilots", # american 
+    "Undertale", # american 
+    "Vampire Diaries Universe", # american 
+    "Victorious", # american 
+    "Voltron", # american 
+    "Warrior Nun", # american 
+    "Welcome to Night Vale", # american from what I gather but again no explicit info other than their (fan??) subreddit???
+    "White Collar", # american 
+    "Xena", # american 
+    "Drag", # RPF I looked them up they're all american/it's all US version of drag race
+    "Women's Soccer", # RPF they're all US players
+]
+british_fandoms = [
+    "Doctor Who",
+    "Torchwood", # shouldn't this be doctor who universe???? FIX THAT
+    "Emmerdale",
+    "Harry Potter Universe",
+    "Heartstopper",
+    "Lord of the Rings Universe",
+    "One Direction",
+    "Sherlock",
+    "Holby City", # british
+    "James Bond Universe", # british
+    "Killing Eve", # british
+    "Kingsman", # british
+    "Skins", # british
+    "The Magnus Archives", # british from what I can tell but doesn't say much of anywhere smh
+    "The Professionals", # british (old af wtf)
+    "The Sandman", # british
+    "Life on Mars", # british
+    "Lockwood & Co.", # british
+    "Merlin", # british
+    "Motherland", # british
+    "Good Omens", # neil gaiman bri'ish
+]
+canadian_fandoms = [
+    "Rookie Blue", # canadian
+    "Orphan Black", # canadian
+    "Mass Effect", # canadian creator
+    "Lost Girl", # canadian
+    "Dragon Age", # canadian
+    "Due South", # canadian
+    "Carmilla", # supposing this is the 2014 web series (it is): canadian, original carmilla novel is irish
+]
+australian_fandoms = [
+    "Wentworth", # australian
+    "5 Seconds of Summer", # australian!
+]
+japanese_fandoms = [
+    "Attack on Titan | 進撃の巨人",
+    "BLUELOCK | ブルーロック",
+    "Bungou Stray Dogs | 文豪ストレイドッグス",
+    "Dangan Ronpa",
+    "Final Fantasy | ファイナルファンタジー",
+    "Fire Emblem | ファイアーエムブレム",
+    "Free!",
+    "Fullmetal Alchemist | 鋼の錬金術師",
+    "Haikyuu!! | ハイキュー!!",
+    "Hatsune Miku / Vocaloid | 初音ミク / ボーカロイド",
+    "Hetalia | ヘタリア",
+    "JoJo's Bizarre Adventure | ジョジョの奇妙な冒険",
+    "Jujutsu Kaisen | 呪術廻戦",
+    "Kill la Kill | キルラキル",
+    "Little Witch Academia | リトルウィッチアカデミア",
+    "Love Live! | ラブライブ!",
+    "Mobile Suit Gundam Wing | 新機動戦記ガンダム W",
+    "My Hero Academia | 僕のヒーローアカデミア",
+    "One Piece | ワンピース",
+    "One-Punch Man | ワンパンマン",
+    "Pretty Guardian Sailor Moon | 美少女戦士セーラームーン",
+    "Puella Magi Madoka Magica | 魔法少女まどか☆マギカ",
+    "SK8 the Infinity | SK∞ エスケーエイト",
+    "Seraph of the End | 終わりのセラフ",
+    "Tokyo Ghoul | 東京喰種",
+    "Tokyo Revengers | 東京卍リベンジャーズ",
+    "Trigun Universe | トライガン",
+    "Yuri!!! on ICE | ユーリ!!! on ICE",
+    "Persona",
+    "Naruto",
+    "KAT-TUN", # カトゥーン japanese boy band! (add the dang kana)
+]
+korean_fandoms = [
+    "Bangtan Boys / BTS",
+    "EXO",
+    "NCT",
+    "Omniscient Reader | 전지적 독자 시점",
+    "Stray Kids",
+    "TOMORROW X TOGETHER / TXT",
+    "Red Velvet",
+]
+chinese_fandoms = [
+    "Genshin Impact | 原神",
+    "Grandmaster of Demonic Cultivation / The Untamed | 魔道祖师 / 陈情令",
+    "Heaven Official's Blessing | 天官赐福",
+    "New Gods | 新神榜",
+    "Word of Honor | 山河令",
+    "Journey to the West Universe",
+    "Super-Vocal | 声入人心", # chinese (I'm guessing mandarin??? it just says chinese and I can't tell)
+    "MIRROR", # hong kong cantopop! we got cantonese rep!!!!
+]
+french_fandoms = [
+    "Miraculous: Tales of Ladybug & Cat Noir | Miraculous: Les Aventures de Ladybug et Chat Noir", # french
+]
+other_fandoms = {
+    "Young Royals":"Sweden",
+    "The Witcher | Wiedźmin":"Poland",
+    "The Locked Tomb / Gideon the Ninth": "New Zealand", 
+        # (I mean this makes sense bc they're maori innit)
+    "SKAM": "Norway", 
+    "KinnPorsche | คินน์พอร์ช เดอะ ซีรีส์":"Thailand", 
+    "All For The Game":"Unknown", 
+        # I have no clue 
+        # this is some rando tumblr user's novel series, 
+        # doesn't even have a wikipedia article, 
+        # and author moves around a lot, no info on where she's from smh
+
+    "Power Rangers": "Japan / USA", 
+    "Wynonna Earp": "Canada / USA", 
+    "Kingdom Hearts": "Japan / USA", 
+    "Highlander": "UK / USA",
+    "Arcane": "France / USA",
+    "Detroit: Become Human": "France / USA", # french studio somehow but hell yeah more french rep they recorded in us tho
+    "Life Is Strange": "France / Japan", # ANOTHER FRENCH STUDIO, 
+        # but under square enix which is japanese?? & has branches across several continents so uh
+    "Les Misérables": "France / UK / USA", # technically french??? but the movie wasn't??? movie is us-uk
+    "Carol": "UK / USA", # UK and US, original story is american, we'll go with that I guess
+
+}
+
+real_human_americans = [
+    'Patrick Kane', # american
+    'Alexander | Technoblade',
+    'Clay | Dream', # american
+    'Darryl Noveschosch | BadBoyHalo', # american
+    'Karl Jacobs', # american
+    'Kristin Watson, née Rosales', # american
+    'Mark Fischbach | Markiplier', # american
+    'Michael Jones', # american
+    'Nicholas Armstrong | Sapnap', # american
+    'Ranboo', # american
+    'Ryan | GoodTimesWithScar', # american
+    'Zak Ahmed | Skeppy', # american
+]
+real_human_brits = [
+    'Charles | Grian', # UK
+    'Dan Howell | danisnotonfire', # UK
+    'Gavin Free', # UK
+    'George Davidson | GeorgeNotFound', # UK
+    'Phil Lester | AmazingPhil', # UK
+    'Phil Watson | Philza', # UK
+    'Thomas Michael Simons | TommyInnit', # UK
+    'Toby Smith | Tubbo', # UK
+    'William Patrick Spencer Gold | Wilbur Soot', # UK
+]
+other_real_humans = {
+    # sean will go here cause he irish
+    'Boyang Jin': "China", # chinese
+    'Yuzuru Hanyu': "Japan", # japanese
+    'Jonathan Toews': "Canada", # canadian
+    'Sean McLoughlin | Jacksepticeye': "Ireland", # irish
+    "Alexis 'Alex' Maldonado | Quackity": "Mexico", # mexican
+}
 
 def add_countries_of_origin_and_languages(input_list):
 
     fandom_list = deepcopy(input_list)
+    new_list = []
 
-    country_languages = {
+    country_languages = { # minus english & chinese
         "South Korea": "Korean",
         "Japan": "Japanese",
         "France": "French",
-        # do china separately cause of mandarin vs cantonese, 
-        # although I'm guessing everything will be mandarin
-
-        # look up if thai show is in thai or if language is called smth else??
-
-        # any other countries left??
+        "Poland": "Polish",
+        "Sweden":"Swedish",
+        "Norway": "Norwegian",
+        "Thailand": "Thai",
     }
-    
-    new_list = []
 
     for fandom_dict in fandom_list:
         fandom = fandom_dict["fandom"]
 
-        if fandom in american_fandoms:
+        if fandom in american_fandoms or fandom_dict["instance"] in real_human_americans:
             fandom_dict["country_of_origin"] = "USA"
-        elif fandom in british_fandoms:
+        elif fandom in british_fandoms or fandom_dict["instance"] in real_human_brits:
             fandom_dict["country_of_origin"] = "UK"
         elif fandom in japanese_fandoms:
             fandom_dict["country_of_origin"] = "Japan"
@@ -38,15 +296,40 @@ def add_countries_of_origin_and_languages(input_list):
             fandom_dict["country_of_origin"] = "South Korea"
         elif fandom in chinese_fandoms:
             fandom_dict["country_of_origin"] = "China"
+        elif fandom in australian_fandoms:
+            fandom_dict["country_of_origin"] = "Australia"
+        elif fandom in french_fandoms:
+            fandom_dict["country_of_origin"] = "France"
+        elif fandom in canadian_fandoms:
+            fandom_dict["country_of_origin"] = "Canada"
         elif fandom in other_fandoms:
             fandom_dict["country_of_origin"] = other_fandoms[fandom]
-        else: print(fandom)
+        elif fandom_dict["instance"] in other_real_humans:
+            fandom_dict["country_of_origin"] = other_real_humans[fandom_dict["instance"]]
+        else: print(f"'{fandom}',") # if we haven't assigned a country
 
-        if fandom_dict["country_of_origin"] in ["USA", "UK", "Canada", "Australia"]:
+        if fandom_dict["country_of_origin"] in [ # english speaking countries
+            "USA", 
+            "UK", 
+            "Canada", 
+            "Australia", 
+            "New Zealand", 
+            "Ireland"
+        ] \
+        or fandom in ["Life Is Strange", "All For The Game"] \
+        or "USA" in fandom_dict["country_of_origin"] \
+        or fandom_dict["instance"] in ["Alexis 'Alex' Maldonado | Quackity"]:
             fandom_dict["original_language"] = "English"
-        if fandom_dict["country_of_origin"] in country_languages:
+        elif fandom_dict["country_of_origin"] == "China":
+            if fandom == "MIRROR":
+                fandom_dict["original_language"] = "Chinese (Cantonese)"
+            else:
+                fandom_dict["original_language"] = "Chinese" 
+                # I can't figure out how to confirm whether smth is mandarin or no, 
+                # so we're going w just chinese
+        elif fandom_dict["country_of_origin"] in country_languages:
             fandom_dict["original_language"] = country_languages[fandom_dict["country_of_origin"]]
-        else: print(fandom_dict["country_of_origin"])
+        else: print(f"'{fandom_dict['country_of_origin']}': ,") # if we haven't assigned a language
 
         new_list.append(fandom_dict)
     
