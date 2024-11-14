ae35a42c345abb82f9cbdd1f54340e77386d2458
dkettchen <dkettchen@gmail.com>
2024-11-14 09:27:05 +0000

    implement helper func to join fandom info to df

--- visualisation/input_data_code/make_joined_ranking_df.py
+++ visualisation/input_data_code/make_joined_ranking_df.py
@@ -1,6 +1,10 @@
 from visualisation.input_data_code.make_file_dfs import make_yearly_df_dict
 from visualisation.input_data_code.edit_columns import edit_ranking_df_columns
 from visualisation.input_data_code.join_additional_data import join_additional_data
+from visualisation.vis_utils.remove_member_columns import remove_members_from_df
+from visualisation.vis_utils.join_member_info import join_character_info_to_df
+import pandas as pd
+
 
 def make_joined_ranking_df(ranking:str):
     """
@@ -16,4 +20,25 @@ def make_joined_ranking_df(ranking:str):
     ship_joined_df = join_additional_data(edited_df_dict, "ships", ranking) 
 
     return ship_joined_df
+
+def make_fandom_joined_df(ship_joined_df:pd.DataFrame, data_case:str):
+    """
+    takes the output of make_joined_ranking_df
+
+    removes members if data_case="ships", joins member info if data_case="characters"
+
+    then joins data from additional fandom data & world population data files
+
+    returns joined dataframe
+    """
+
+    if data_case == "ships":
+        base_df = remove_members_from_df(ship_joined_df)
+    elif data_case == "characters":
+        base_df = join_character_info_to_df(ship_joined_df)
+
+    with_fandoms_df = join_additional_data(base_df, "fandom")
+    with_fandoms_and_pop_df = join_additional_data(with_fandoms_df, "population")
+
+    return with_fandoms_and_pop_df
 
