3c3f4086fbc8c15797c662e21006e72ca41c5697
dkettchen <dkettchen@gmail.com>
2025-05-18 11:29:28 +0100

    assign data to characters

--- src/additional_data_fandoms/sort_extra_ship_data.py
+++ src/additional_data_fandoms/sort_extra_ship_data.py
@@ -13,7 +13,7 @@ from src.util_functions.conflicting_orientations import conflicting_orientations
 
 def parse_extra_ship_data():
     """
-    uses data from (additional ship data, character & ship files)
+    uses data from (additional ship data & ship files) to create a df
 
     - removes any ships that were only represented as a non-slash ship
     - removes any multi-ships as there were no canon multi ships 
@@ -25,7 +25,6 @@ def parse_extra_ship_data():
     new_data = df_from_csv(new_data_path).set_index("slash_ship")
     new_data.pop("fandom")
 
-    character_df = make_characters_df()
     ship_df = make_ships_df()
 
     joined_ship_df = ship_df.join(other=new_data, on="slash_ship", lsuffix="_left", rsuffix="_right")
@@ -53,111 +52,47 @@ def parse_extra_ship_data():
     ).dropna(how="all")
     joined_ship_df.pop("members_no") # they're all 2 now
 
-    # # assign orientations to characters 
-    # # (and remove characters not represented in relevant ships - currently removes 13 chars)
-    # oriented_chars = assign_orientations(joined_ship_df, character_df)
+    return joined_ship_df
 
-    # # now cross reference with gender! 
-    # # -> adds more specific columns based on gender (ie woman/man attracted etc)
-    # specified_orientations_char_df = cross_reference_with_gender(oriented_chars)
+def assign_to_characters(joined_ship_df):
+    """
+    processes and adds orientation data to the characters 
+    (ex which characters are attracted to men and/or women, which characters are canon queer, etc)
+    """
+    character_df = make_characters_df()
 
+    # assign orientations to characters 
+    # (and remove characters not represented in relevant ships - currently removes 13 chars)
+    oriented_chars = assign_orientations(joined_ship_df, character_df)
 
+    # now cross reference with gender! 
+    # -> adds more specific columns based on gender (ie woman/man attracted etc)
+    specified_orientations_char_df = cross_reference_with_gender(oriented_chars)
+
+    return specified_orientations_char_df
+
+def assign_to_ships(joined_ship_df):
+    """
+    adds info on whether canon orientation conflicts or aligns with the ship type (or is ambiguous/unspecified)
+
+    it also adds helpful columns on whether ships are mlm wlw het or other 
+    (to include say M | Other characters in mlm)
+    """
+    
     # now to compare which ships are conflicting w canon orientations & which aren't
     conflicts_ship_df = conflicting_orientations(joined_ship_df)
 
-    # canon should only be ["Yes", "No", "One-sided", "fanon"] (and the former 3 with *)
-    # print(new_data["canon"].unique())
+    return conflicts_ship_df
 
-    # incest should only be ["Yes", "No", "step", "foster", "adoptive", "in-law"] (ditto)
-    # print(new_data["related"].unique())
+# TODO print new files?
 
-    # orientation should be the following combos and a few wild cards we can print
-    # [
-    #     "unspecified",
-    #     "str8/unspecified",
-    #     "unspecified/str8",
-    #     "gay/unspecified",
-    #     "unspecified/gay",
-    #     "bi/unspecified",
-    #     "unspecified/bi",
-    #     "str8/str8",
-    #     "gay/str8",
-    #     "str8/gay",
-    #     "bi/str8",
-    #     "str8/bi",
-    #     "gay/gay",
-    #     "gay/bi",
-    #     "bi/gay",
-    #     "bi/bi",
+# canon should only be ["Yes", "No", "One-sided", "fanon"] (and the former 3 with *)
+# print(new_data["canon"].unique())
 
-    #     # some with acearo
-    #     # unspecified doctor & canon nb that are woman_attracted
-    #     # some of these ones but with * in there
-    # ]
-    # [
-    #     'str8/str8',
-    #     'str8/bi', 
-    #     'str8/unspecified',
-    #     'str8*/str8',
-    #     'str8/str8*', 
-    #     'str8/gay', 
-    #     'str8*/gay', 
-    #     'str8/gay*', 
-    #     'str8/acearo', 
-    #     'str8*/unspecified', 
-    #     'str8/fanon', 
-    #     'str8/bi*', 
-    #     'str8/unspecified*'
-
-    #     'bi/gay', 
-    #     'bi/bi*', 
-    #     'bi/bi', 
-    #     'bi/str8', 
-    #     'bi/woman_attracted',
-    #     'bi/unspecified', 
-    #     'bi/str8*',
-    #     'bi*/str8', 
-
-    #     'unspecified', 
-    #     'unspecified/str8',
-    #     'unspecified/gay',
-    #     'unspecified*/str8', 
-    #     'unspecified/bi', 
-    #     'unspecified/acearo', 
-
-    #     'gay/str8', 
-    #     'gay/gay', 
-    #     'gay/unspecified',  
-    #     'gay/gay*', 
-    #     'gay/str8*', 
-    #     'gay/bi', 
-    #     'gay/unspecified*', 
-        
-    # ]
-
-    # make columns by crossreferencing for
-
-    # "canon_man_attracted", 
-    # "canon_woman_attracted", (all bis except for eda)
-    # "canon_other_attracted", (only eda)
-    # "canon_acearo", (only 2)
-    # "unspecified_orientation",
-
-    # then cross reference??
-    # "canon_str8",
-    # "canon_queer",
-    # "canon_bi",
-
-    # all should have a / in there or be "unspecified"
-    # print(new_data["canon_orientation"].unique())
-
-    pass
-
-def assign_to_characters():
-    pass
-
-def assign_to_ships():
-    pass
+# incest should only be ["Yes", "No", "step", "foster", "adoptive", "in-law"] (ditto)
+# print(new_data["related"].unique())
 
 if __name__ == "__main__":
     parsed_df = parse_extra_ship_data()
+    assigned_char_df = assign_to_characters(parsed_df)
+    assigned_ship_df = assign_to_ships(parsed_df)
