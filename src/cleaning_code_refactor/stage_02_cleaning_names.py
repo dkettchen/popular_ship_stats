ba654435793e8fe0a5290f8526c88c903d1498a1
dkettchen <dkettchen@gmail.com>
2025-05-23 19:11:55 +0100

    split og names into parts

--- src/cleaning_code_refactor/stage_02_cleaning_names.py
+++ src/cleaning_code_refactor/stage_02_cleaning_names.py
@@ -3,23 +3,16 @@ import pandas as pd
 from src.cleaning_code_refactor_utils.gather_chars_and_fandoms import gather_raw_chars_and_fandoms
 from src.cleaning_code_refactor_utils.find_RPF import find_RPF
 from src.cleaning_code_refactor_utils.clean_fandom_labels import clean_fandoms
+from src.cleaning_code_refactor_utils.clean_char_names import clean_names
 from json import dump
 
 def clean_names(parsed_dict:dict):
     ## fourth stage cleaning 
 
-    # collecting names
+    # collecting all raw fandom & char names
     raw_fandoms_and_chars = gather_raw_chars_and_fandoms(parsed_dict)
 
-    # TODO
-            # reference unify fandoms file
-        # clean fandoms
-        # then make a fandom df w new names, old names & indexes
-
-
-            # return to referencing separate names into parts file
-        # combine characters per new bigger fandoms
-        # clean characters
+    ## fandoms
 
     # extracting fandoms
     fandom_df = pd.DataFrame(sorted(list(raw_fandoms_and_chars.keys())), columns=["Fandom"])
@@ -30,12 +23,40 @@ def clean_names(parsed_dict:dict):
     fandom_df = fandom_df.rename(columns={
         "Fandom": "Old Fandom", 
         "New Fandom": "Fandom"
-    }).sort_values("Fandom")
+    }).set_index("Old Fandom").sort_values("Fandom")
 
-    # print all fandoms to a list to look at em
+    # print all fandoms to a list to look at em (currently all fandoms up to 2024 are parsed correctly)
     all_fandoms = list(fandom_df["Fandom"].unique())
     with open("data/reference_and_test_files/refactor_helper_files/fandoms.json", "w") as fandoms_json:
         dump(all_fandoms, fandoms_json, indent=4)
+
+    # combine characters per new fandom
+    new_fandoms_and_chars = {fandom: set() for fandom in all_fandoms}
+    for old_fandom in fandom_df.index:
+        new_fandom = fandom_df["Fandom"].loc[old_fandom]
+        characters = raw_fandoms_and_chars[old_fandom]
+        for char in characters:
+            new_fandoms_and_chars[new_fandom].add(char)
+    for key in new_fandoms_and_chars:
+        new_fandoms_and_chars[key] = sorted(list(new_fandoms_and_chars[key]))
+
+    ## characters
+
+    # make a char name df
+    char_names = []
+    for fandom in new_fandoms_and_chars:
+        for char in new_fandoms_and_chars[fandom]:
+            char_dict = {
+                "Old Name": char,
+                "Fandom": fandom
+            }
+            char_names.append(char_dict)
+    char_df = pd.DataFrame(char_names)
+
+    # clean names
+    char_df["New Name"] = char_df["Old Name"].apply(clean_names)
+
+
 
 
 
---
+++ src/cleaning_code_refactor_utils/clean_char_names.py
@@ -0,0 +1,113 @@
+from re import split
+
+def remove_brackets(old_name:str):
+    """
+    if there are brackets in the given name,
+    it removes everything from the whitespace before the brackets 
+    and returns the remaining name
+
+    ex "Arthur Pendragon (Merlin)" -> "Arthur Pendragon"
+    """
+
+    if "(" in old_name:
+        new_name = split(" \(", old_name)[0]
+    else: 
+        new_name = old_name
+
+    return new_name
+def separate_into_parts(old_name:str):
+    """
+    separates the given name into the contained name parts
+    """
+
+    if " | " in old_name:
+        split_name = split(r" \| ", old_name)
+    else: 
+        split_name = [old_name]
+
+    new_name = []
+
+    specific_cases = {
+        "Rap Monster": ["Rap Monster/RM"], 
+        "RM": ["Rap Monster/RM"],
+        'Rumpelstiltskin': ["Rumpelstiltskin"],
+        'Rumplestiltskin': ["Rumpelstiltskin"],
+        "Geralt z Rivii": ["Geralt","z Rivii"],
+        "Helena 'H. G.' Wells": ["Helena","'H. G.'","Wells"],
+        "You": ["Reader"],
+    }
+    multi_word_names = [
+        'Chat Noir',
+        #'Darth Vader',
+        #'Captain Hook',
+        'Evil Queen',
+        'The Golden Guard',
+        'The Archivist',
+        'Six-eared Macaque',
+        #'Madam Satan',
+        'My Unit', # I think this is not a name, but tbh look up, other name was 'Byleth'
+        'Red Riding Hood',
+        'Cherry Blossom',
+        'Soldier: 76',
+        'Monkey King',
+        'The Darkling',
+        'All Might',
+        'Present Mic',
+        #'Kylo Ren',
+        "Ninth Doctor",
+        "Persona 5 Protagonist",
+        #"Princess Bubblegum",
+        "Pink Diamond",
+        "Rose Quartz",
+        #"Mr. Gold",
+        "Tenth Doctor",
+        "The Doctor",
+        "Thirteenth Doctor",
+        "Twelfth Doctor",
+        "Eleventh Doctor",
+        "Upgraded Connor",
+        "Venom Symbiote",
+        "Lapis Lazuli", 
+        "Iron Bull",
+        "Wilbur Soot"
+    ]
+
+    for part in split_name:
+        new_part = None
+
+        # "<blank> of <blank>" et al
+        for conn in [
+            " of ",
+            " di ",
+            " al ",
+            " Van ",
+            " von ",
+            " the ",
+        ]:
+            if conn in part:
+                temp_split = split(conn, part)
+                new_part = [temp_split[0], conn + temp_split[1]]
+                break
+
+        if not new_part:
+            if part in specific_cases: # special cases dict
+                new_part = specific_cases[part]
+            elif ("Female " in part or "Male " in part) \
+            or part not in multi_word_names: # just split at white spaces
+                new_part = split(r"\s", part)
+            else: # do not split
+                new_part = [part]
+
+        # add separated parts to new name
+        new_name += new_part
+
+    return new_name
+
+def clean_names(old_name:str):
+    """
+    removes brackets & splits name string into a list of name parts
+    """
+    new_name = remove_brackets(old_name)
+    new_name = separate_into_parts(new_name)
+
+    return new_name
